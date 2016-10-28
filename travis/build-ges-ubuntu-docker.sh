50cec5accc23c259340e85b2b316d61cb6e3001e
ProfessorKaos64 <ProfessorKaos64@users.noreply.github.com>
2016-10-28 17:44:17 -0400

    Update build-ges-ubuntu-docker.sh

--- travis/build-ges-ubuntu-docker.sh
+++ travis/build-ges-ubuntu-docker.sh
@@ -2,8 +2,19 @@
 
 # Instructions for docker w/ Travis CI build
 
-apt-get udpate -y
-apt-get install -y cmake libboost-dev libc6-dev git
+cat<<- EOF
+
+------------------------------
+Starting Ubuntu build test
+------------------------------
+
+EOF
+
+sleep 2s
+ls
+pwd
+apt-get update -qq
+apt-get install -yqq cmake libboost-dev libc6-dev git
 git submodule update --init
 rm -rf build && mkdir build
 cd build && cmake -DCMAKE_INSTALL_PREFIX=/usr ..
