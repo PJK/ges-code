fdc03844a3a0ce36855a62d0b51d6e5fff85ae10
ProfessorKaos64 <ProfessorKaos64@users.noreply.github.com>
2016-10-29 09:48:26 -0400

    Update build-ges-ubuntu-docker.sh

--- travis/build-ges-ubuntu-docker.sh
+++ travis/build-ges-ubuntu-docker.sh
@@ -9,7 +9,7 @@ sleep 2s
 ######################
 
 apt-get update -qq
-apt-get install -qq cmake gcc g++ git libboost-dev libldap2-dev nis
+apt-get install -qq cmake gcc g++ git libboost-dev libldap2-dev libnis-dev
 git submodule init --recursive
 
 ######################
